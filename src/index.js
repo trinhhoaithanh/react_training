25ff50e073cdb8650bab28df213b080c23ffafcf
ththanh <hoaithanhtrinh2002@gmail.com>
2022-11-04 21:53:29 +0700

    dp

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import BaiTapThucHanhLayout from './components/BaiTapThucHanhLayout/BaiTapThucHanhLayout';
+import BaiTapThucHanhLayout from './components/BTLayout/BaiTapThucHanhLayout';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
